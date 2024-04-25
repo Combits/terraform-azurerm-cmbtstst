aba41c542f1da5014cbf2c7cb9b38904adcc056c
Combits <info@combits.be>
2024-04-25 20:30:46 +0200

    v1.0.2

--- main.tf
+++ main.tf
@@ -5,15 +5,15 @@ locals {
     "Environment" = var.environment
   }
 }
-resource "azurerm_resource_group" "rg" {
-  name     = "rg_terraform_sandbox"
-  location = "westeurope"
-}
+#resource "azurerm_resource_group" "rg" {
+#  name     = "rg_terraform_sandbox"
+#  location = "westeurope"
+#}
 resource "azurerm_storage_account" "cmbtstst" {
   resource_group_name           = var.resource_group_name
   name                          = var.storage_account_name
-  location                      = azurerm_resource_group.rg.location
-  account_replication_type      = var.environment == "Production" ? "ZRS" : "LRS"
+  location                      = var.location
+  account_replication_type      = var.environment == "Production" ? "GRS" : "LRS"
   account_tier                  = "Standard"
   public_network_access_enabled = false
   tags                          = local.tags
